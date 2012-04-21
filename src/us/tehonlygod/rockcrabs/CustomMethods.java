91cea39fffe938770aa9489f15cd7b2133eea698
tehonlygod <tehonlygod@gamingautomated.com>
2012-04-21 00:34:24 -0500

    added packaging

---
+++ src/us/tehonlygod/rockcrabs/CustomMethods.java
@@ -0,0 +1,214 @@
+package us.tehonlygod.rockcrabs;
+
+
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Point;
+
+import org.powerbot.game.api.methods.Calculations;
+import org.powerbot.game.api.methods.Walking;
+import org.powerbot.game.api.methods.Widgets;
+import org.powerbot.game.api.methods.input.Mouse;
+import org.powerbot.game.api.methods.interactive.Players;
+import org.powerbot.game.api.methods.node.SceneEntities;
+import org.powerbot.game.api.methods.tab.Inventory;
+import org.powerbot.game.api.util.Filter;
+import org.powerbot.game.api.util.Random;
+import org.powerbot.game.api.util.Time;
+import org.powerbot.game.api.wrappers.Tile;
+import org.powerbot.game.api.wrappers.interactive.Player;
+import org.powerbot.game.api.wrappers.node.Item;
+import org.powerbot.game.api.wrappers.node.SceneObject;
+
+public class CustomMethods {
+
+	public static SceneObject findClosestSceneObjectObject(int id){
+		Player user = Players.getLocal();
+		double ShortestDistance = Double.MAX_VALUE;
+		SceneObject returnValue = null;
+		for(SceneObject l : SceneEntities.getLoaded()){
+			if(l.getId() == id && ShortestDistance > Calculations.distance(user.getLocation(), l.getLocation())){
+				ShortestDistance = Calculations.distance(user.getLocation(), l.getLocation());
+				returnValue = l;
+			}
+		}
+		return returnValue;
+	}
+	public static void PrintoutMouseSceneObject(){
+		System.out.println("X: " + Mouse.getX() + " Y: " + Mouse.getY());
+	}
+	
+	public static int getNumberInStack(int id){
+		for(Item i : Inventory.getItems()){
+			if(i.getId() == id)
+				return i.getWidgetChild().getChildStackSize();
+		}
+		return 0;
+	}
+	
+	public static void PaintMouse(Graphics2D g){
+		g.setColor(Color.CYAN);
+		g.drawLine(Mouse.getX()-5, Mouse.getY(), Mouse.getX()+5, Mouse.getY());
+		g.drawLine(Mouse.getX(), Mouse.getY()-5, Mouse.getX(), Mouse.getY()+5);
+	}
+	public static void TraverseOld(Tile[] t){
+		int currentPoint = getNextTileOnPath(t);
+		while(currentPoint <= t.length-1){
+			Tile tmpTile = new Tile(t[currentPoint].getX()+Random.nextGaussian(-1, 0, 2), t[currentPoint].getY()+Random.nextGaussian(-1, 0, 2), 0);
+			if(Calculations.distance(Players.getLocal().getLocation(), tmpTile) < 14){
+				Energy();
+				if(Walking.walk(tmpTile))
+					currentPoint++;
+
+				Time.sleep(Random.nextInt(800,900));
+			} else return;
+		}
+	}
+	public static void Traverse(Tile[] t){
+		Tile walkHere = getNextTileOnPathReturnsTile(t);
+		while(Calculations.distance(walkHere, Players.getLocal().getLocation())>2){
+				Energy();
+				Walking.walk(walkHere);
+				Time.sleep(Random.nextInt(800,900));
+				walkHere = getNextTileOnPathReturnsTile(t);
+		}
+	}
+	public static Tile getNextTileOnPathReturnsTile(Tile[] t){
+		Player user = Players.getLocal();
+		for(int i = t.length-1; i >= 0; i--){
+			if(Calculations.distance(t[i], user.getLocation()) < 14){
+				return t[i];
+			}
+		}
+		return null;
+		/*for(int i = 0; i < t.length-1; i++){
+			if(Calculations.distance(user.getLocation(), t[i]) < 14){
+				if(Calculations.distance(user.getLocation(), t[i+1]) > 14){
+					return t[i];
+				}
+			}
+		}
+		if(Calculations.distance(user.getLocation(), t[t.length -1]) <18){
+			return t[t.length-1];
+		}
+		return t[-1];*/
+	}
+	public static int getNextTileOnPath(Tile[] t){
+		Player user = Players.getLocal();
+		for(int i = 0; i < t.length-1; i++){
+			if(Calculations.distance(user.getLocation(), t[i]) < 18){
+				if(Calculations.distance(user.getLocation(), t[i+1]) > 18){
+					return i;
+				}
+			}
+		}
+		if(Calculations.distance(user.getLocation(), t[t.length -1]) <18){
+			return t.length-1;
+		}
+		return -1;
+	}
+
+	public static int getCountOfItem(final int id){
+		int count = Inventory.getCount(new Filter<Item>() {
+
+			@Override
+			public boolean accept(Item i) {
+
+				return i.getId() == id;
+			}
+		});
+
+		return count;
+	}
+
+	public static void Energy(){
+		int r = Random.nextInt(15, 20);
+		if(Walking.isRunEnabled() == false){
+			Walking.setRun(true);
+		}
+		if(Walking.getEnergy() < r){
+			Widgets.get(750, 2).interact("Rest");
+			while(Walking.getEnergy() != 100 && !Players.getLocal().isInCombat()){
+				Time.sleep(50);
+			}
+		}
+	}
+
+	public static int getFoodID(String s){
+		if(s.equalsIgnoreCase("shark")){
+			return 385;
+		} else if(s.equalsIgnoreCase("lobster")){
+			return 379;
+		} else if(s.equalsIgnoreCase("monkfish")){
+			return 7946;
+		} else if(s.equalsIgnoreCase("swordfish")){
+			return 373;
+		} else if(s.equalsIgnoreCase("trout")){
+			return 333;
+		} else if(s.equalsIgnoreCase("tuna")){
+			return 361;
+		} else if(s.equalsIgnoreCase("salmon")){
+			return 329;
+		}
+		return 0;
+
+	}
+	private static Point getCorrectTeleportWidget(String s){
+		if(s.equals("seers")){
+			return new Point(189,156);
+		} else if (s.equalsIgnoreCase("catherby")){
+			return new Point(220,181);
+		} else if (s.equalsIgnoreCase("edgeville")){
+			return new Point(292, 155);
+		} else if (s.equalsIgnoreCase("lumbridge")){
+			return new Point(324, 237);
+		} else if(s.equalsIgnoreCase("draynor")){
+			return new Point(307,210);
+		} else if(s.equalsIgnoreCase("burthorpe")){
+			return new Point(249,146);
+		} else if(s.equalsIgnoreCase("ardougne")){
+			return new Point(165,213);
+		} else if (s.equalsIgnoreCase("al kharid")){
+			return new Point(361,266);
+		} else if (s.equalsIgnoreCase("bandit camp")){
+			return new Point(325,292);
+		} else if (s.equalsIgnoreCase("falador")){
+			return new Point(276,203);
+		} else if (s.equalsIgnoreCase("port sarim")){
+			return new Point(275,246);
+		} else if (s.equalsIgnoreCase("taverly")){
+			return new Point(252, 180);
+		} else if(s.equalsIgnoreCase("varrock")){
+			return new Point(344,177);
+		} else if(s.equalsIgnoreCase("yanile")){
+			return new Point(158,261);
+		}else if (s.equalsIgnoreCase("lunar isle")){
+			return new Point(43, 76);
+		} else {
+			return new Point();
+		}
+
+	}
+	public static void teleportHome(String teleportSceneObject){
+		if(Players.getLocal().getAnimation() == -1){
+			Widgets.get(548, 96).click(true);
+			Time.sleep(500);
+
+			if(Widgets.get(192, 15).getTextureId() != 1703){
+				Widgets.get(192, 15).click(true);
+				Time.sleep(200);
+			} else {
+				Mouse.click(574, 230, true);
+				Time.sleep(1000);
+			}
+
+			Mouse.move((int)getCorrectTeleportWidget(teleportSceneObject).getX(),(int) getCorrectTeleportWidget(teleportSceneObject).getY());
+			Mouse.click((int)getCorrectTeleportWidget(teleportSceneObject).getX(),(int) getCorrectTeleportWidget(teleportSceneObject).getY(), true);
+			Time.sleep(10000);
+			Widgets.get(548,93).click(true);
+			Time.sleep(1000);
+		}
+	}
+
+}
---
+++ src/us/tehonlygod/rockcrabs/RockCrabs.java
@@ -0,0 +1,191 @@
+package us.tehonlygod.rockcrabs;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
+import org.powerbot.game.api.ActiveScript;
+import org.powerbot.game.api.Manifest;
+import org.powerbot.game.api.methods.tab.Skills;
+import org.powerbot.game.bot.event.listener.PaintListener;
+
+
+
+@Manifest(name = "Flawless Rock Crabs", authors = "TehOnlyGod", version = 1.0, description = "Designed with everyone in mind!", premium = true, website = "")
+public class RockCrabs extends ActiveScript implements
+PaintListener, MouseListener{
+	private transient final long startTime = System.currentTimeMillis();
+	final DecimalFormat df = new DecimalFormat();
+	final DecimalFormatSymbols dfs = new DecimalFormatSymbols();
+	FlawlessRockCrabGUI gui;
+	private int skill = Skills.STRENGTH;
+
+	private int startExperience;
+	private int startConExperience;
+	private RegainAgro regain;
+	@Override
+	protected void setup() {
+
+		gui = new FlawlessRockCrabGUI();
+		gui.setVisible(true);
+
+
+		EatFood eat = new EatFood(gui);
+		provide(eat);
+
+
+
+		WalkToCrabs walkBack = new WalkToCrabs(gui);
+		provide(walkBack);
+
+		KillShit kill = new KillShit(gui);
+		provide(kill);
+		GoGetFood gatherFood = new GoGetFood(gui);
+		provide(gatherFood);
+		
+		regain = new RegainAgro(gui, walkBack);
+		provide(regain);
+		
+		HotFixes fix = new HotFixes(regain);
+		provide(fix);
+		startExperience = Skills.getExperiences()[skill];
+		startConExperience = Skills.getExperiences()[Skills.CONSTITUTION];
+
+	}
+
+	private final Color color1 = new Color(0, 0, 0, 138);
+	private final Color color2 = new Color(0, 204, 204, 21);
+	private final Color color3 = new Color(204, 204, 0, 64);
+	private final Color color4 = new Color(0, 0, 153, 107);
+	private final Color color5 = new Color(0, 204, 255);
+
+	private final Font font1 = new Font("Arial", 1, 11);
+	private final Font font2 = new Font("Arial", 3, 10);
+	private final Font font3 = new Font("Arial", 0, 11);
+
+	private final Color color10 = new Color(0, 0, 0, 180);
+    private final Color color12 = new Color(0, 0, 0);
+    private final Color color13 = new Color(0, 255, 255);
+
+    private final BasicStroke stroke10 = new BasicStroke(2);
+    
+    private final Color color100 = new Color(0, 0, 0);
+
+    private final Font font100 = new Font("Arial", 0, 11);
+
+
+
+    private final Font font10 = new Font("Arial", 3, 14);
+	@Override
+	public void onRepaint(Graphics g1) {
+		Graphics2D g = (Graphics2D)g1;
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		long elapsedTime = System.currentTimeMillis() - startTime;
+		final int totalXp = Skills.getExperiences()[skill]
+				- startExperience;
+		final int hpExp = Skills.getExperiences()[Skills.CONSTITUTION]
+				- startConExperience;
+		String message = "Elapsed time: " + formatTime(elapsedTime) + "";
+		int nextLevelXp = (Skills.getExperienceAt(Skills.getLevel(skill)+2) - Skills.getExperience(skill));
+
+		int xpPerHour = (int) ((3600000.0 / elapsedTime) * totalXp);
+		long nextLevelTime = (long) ((double) nextLevelXp / (double) xpPerHour * 3600000);
+		int nextLevelXpHp = (Skills.getExperienceAt(Skills.getLevel(Skills.CONSTITUTION)+2) - Skills.getExperience(Skills.CONSTITUTION));
+		int xpPerHourHp = (int) ((3600000.0 / elapsedTime) * hpExp);
+		long nextLevelTimeHp = (long) ((double) nextLevelXpHp
+				/ (double) xpPerHourHp * 3600000);
+		
+		dfs.setGroupingSeparator(',');
+		df.setDecimalFormatSymbols(dfs);
+		int i = 1;
+		g.setColor(color1);
+		g.fillRect(380+i, 168, 133+i, 166);
+		g.setColor(color2);
+		g.fillRect(384+i, 172, 125+i, 158);
+		g.setColor(color3);
+		g.fillRect(384+i, 200, 124+i, 131);
+		g.setColor(color4);
+		g.fillRect(384+i, 220, 125+i, 23);
+		g.fillRect(384+i, 265, 125+i, 23);
+		g.fillRect(384+i, 308, 125+i, 23);
+		g.setFont(font1);
+		g.setColor(color5);
+		g.drawString("Flawless Rock Crabs", 390+i, 185);
+		g.setFont(font2);
+		
+		g.drawString("     By TehOnlyGod", 390+i, 194);
+		g.setFont(font3);
+		g.drawString(message, 390+i, 213);
+		g.drawString("Strength XP p/h: "+xpPerHour, 389+i, 235);
+		g.drawString("Hp XP p/h: "+xpPerHourHp, 389+i, 257);
+		g.drawString("Strength XP TNL: " + nextLevelXp, 389+i, 280);
+		g.drawString("TTL Strength: " + formatTime(nextLevelTime), 389+i, 303);
+		g.drawString("TTL Hp: " + formatTime(nextLevelTimeHp), 389+i, 325);
+		g.setColor(color10);
+        g.fillRect(443, 5, 72, 25);
+        g.setColor(color12);
+        g.setStroke(stroke10);
+        g.drawRect(443, 5, 72, 25);
+        g.setFont(font10);
+        g.setColor(color13);
+        g.drawString("Options", 453, 22);
+        
+        g.setFont(font100);
+        g.setColor(color100);
+       // g.drawString("Count: " + regain.getCount(), 421, 470);
+
+		CustomMethods.PaintMouse(g);
+
+	}
+
+	private String formatTime(final long time) {
+		final int sec = (int) (time / 1000), h = sec / 3600, m = sec / 60 % 60, s = sec % 60;
+		return (h < 10 ? "0" + h : h) + ":" + (m < 10 ? "0" + m : m) + ":"
+		+ (s < 10 ? "0" + s : s);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if(e.getX() >= 443 && e.getX() <= 443+72 && e.getY() >= 4 && e.getY()<=5+25){
+			if(gui.isVisible() == false){
+				gui.setVisible(true);
+			}
+		}
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+}
